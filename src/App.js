db9699c7b2bd0633a735db556969cf6e720f58ce
SAIEF BRAHIM <100806401+SAIEFIBRAHIM@users.noreply.github.com>
2022-07-16 18:34:26 +0100

    Finishing the API Checkpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import UserList from "./components/UserList";
 
 function App() {
   return (
-    <div className=" bg-base-100">
+    <div className=" bg-base-100 my-6">
       <UserList />
     </div>
   );
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,25 +1,69 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import { CgProfile } from "react-icons/cg";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { TailSpin } from "react-loader-spinner";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    Axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-      setUsers(response.data);
-    });
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await Axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(res.data);
+      setLoading(false);
+    };
+    fetchData();
   }, []);
 
-  return (
-    <div>
-      {users.map((el, i) => (
-        <div key={i}>
-          <h1>
-            {el.id} {el.name}
-          </h1>
+  if (loading) {
+    <div className=" flex text-5xl justify-center items-center text-accent">
+      <TailSpin ariaLabel="loading-indicator" />;
+    </div>;
+  } else
+    return (
+      <>
+        <div className=" flex text-4xl my-8 justify-center font-medium text-accent ">
+          Users List
         </div>
-      ))}
-    </div>
-  );
+        <div className=" grid grid-flow-row grid-cols-3 gap-8 mx-12">
+          {users.map((el, i) => (
+            <div key={i}>
+              <div className="card w-96 bg-base-300 shadow-xl items-center">
+                <CgProfile
+                  className=" mt-6 text-accent hover:text-accent-focus"
+                  size={80}
+                />
+                <div className="card-body text-center">
+                  <h2 className="card-title justify-center text-info-content">
+                    {el.name}
+                  </h2>
+                  <p>{el.username}</p>
+                  <p>{el.email}</p>
+                  <div className="card-actions flex-col items-center justify-center">
+                    <a
+                      href={`tel:${el.phone}`}
+                      className="badge badge-outline hover:bg-accent hover:text-white hover:border-0"
+                    >
+                      {el.phone}
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={`http://${el.website}`}
+                      className="badge badge-outline hover:bg-accent hover:text-white hover:border-0"
+                    >
+                      Go to Website
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
 };
 
 export default UserList;
